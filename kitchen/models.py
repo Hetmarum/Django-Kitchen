c5641b6b31b05bea51f15efbc54e2cb901380326
Hetmarum <kpxmedet@gmail.com>
2025-08-21 13:29:39 +0300

    added models & auth user model

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ("username", )
+
+    def __str__(self):
+        return f"{self.username}: {self.first_name} {self.last_name}"
+
+
+class DishType(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE, related_name="dishes")
+    cooks = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="dishes")
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return f"{self.name} {self.dish_type} Price: {self.price}"
