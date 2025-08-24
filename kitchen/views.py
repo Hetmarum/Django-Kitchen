4bcacc2bd3ed82d55f59924f56fff6303e4cdd48
Hetmarum <kpxmedet@gmail.com>
2025-08-24 17:17:42 +0300

    misc changes

--- kitchen/views.py
+++ kitchen/views.py
@@ -117,7 +117,7 @@ class DishTypeDeleteView(LoginRequiredMixin, generic.DeleteView):
 
 class DishListView(LoginRequiredMixin, generic.ListView):
     model = Dish
-    paginate_by = 10
+    paginate_by = 6
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
