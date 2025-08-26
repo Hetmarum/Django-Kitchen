f2db99a13d7b6b75ee64da1a61e37ff0c67bedf2
Hetmarum <kpxmedet@gmail.com>
2025-08-26 10:20:58 +0300

    changed on_delete logic for dishes

--- kitchen/models.py
+++ kitchen/models.py
@@ -45,7 +45,10 @@ class Dish(models.Model):
     description = models.TextField(blank=True)
     price = models.DecimalField(decimal_places=2, max_digits=10)
     dish_type = models.ForeignKey(
-        DishType, on_delete=models.CASCADE, related_name="dishes"
+        DishType, on_delete=models.SET_NULL,
+        related_name="dishes",
+        blank=True,
+        null=True
     )
     cooks = models.ManyToManyField(
         settings.AUTH_USER_MODEL,
