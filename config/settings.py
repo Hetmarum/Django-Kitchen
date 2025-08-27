75b741323838e9232d4e47a8142e962722f70cc9
Hetmarum <kpxmedet@gmail.com>
2025-08-27 09:49:23 +0300

    settings changes

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["example.com", "www.example.com", "127.0.0.1"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
